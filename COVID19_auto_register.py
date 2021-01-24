984b8e8734fde3ca03ff778db37954999eb0836b
benjia <benjia.h@qq.com>
2021-01-24 23:04:32 +0800

    optimized detect whether is registered removed some headers optimized process

--- COVID19_auto_register.py
+++ COVID19_auto_register.py
@@ -1,41 +1,29 @@
 import requests
 import datetime
+import re
 
 from urllib import parse
 from time import sleep
 from config import global_config
 from logger import logger
 
+
 def register():
-    url = "https://xsswzx.cdu.edu.cn/ispstu1-2/com_user/project_addx.asp?id={id}&id2={id2}".format(id=global_config.getRaw('config', 'id'), id2=get_date_url())
+    url = "https://xsswzx.cdu.edu.cn/ispstu1-2/com_user/project_addx.asp?id={id}&id2={id2}".format(
+        id=global_config.getRaw('config', 'id'), id2=get_date_url())
     headers = {
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-        "Accept-Encoding": "gzip, deflate, br",
-        "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6",
-        "Connection": "keep-alive",
         "Cookie": global_config.getRaw('config', 'cookie'),
-        "Host": "xsswzx.cdu.edu.cn",
-        "Referer": "https://xsswzx.cdu.edu.cn/ispstu1-2/com_user/project.asp?id={id}".format(id=global_config.getRaw('config', 'id')),
-        "Sec-Fetch-Dest": "frame",
-        "Sec-Fetch-Mode": "navigate",
-        "Sec-Fetch-Site": "same-origin",
-        "Sec-Fetch-User": "?1",
-        "Upgrade-Insecure-Requests": "1",
+        "Referer": "https://xsswzx.cdu.edu.cn/ispstu1-2/com_user/project.asp?id={id}".format(
+            id=global_config.getRaw('config', 'id')),
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36 Edg/88.0.705.50"
     }
     res = requests.get(url=url, headers=headers)
     res.encoding = "utf-8"
     if res.status_code == 200:
-        logger.info("register GET request successfully.Status code:{code}".format(code=res.status_code))
+        logger.info("GET request successfully. URL:{url}. Status code:{code}".format(url=url, code=res.status_code))
     else:
-        logger.error("register GET request failed.Status code:{code}".format(code=res.status_code))
-    if  "已存在" in res.text:
-        logger.info("Register is alread existed.")
-        return True, res.text
-    else:
-        logger.info("Register is not existed.")
-        return False, res.text
-    # return res.text
+        logger.error("GET request failed. URL:{url}. Status code:{code}".format(url=url, code=res.status_code))
+    return res.text
 
 
 def send_wechat(title, message):
@@ -47,9 +35,12 @@ def send_wechat(title, message):
     }
     res = requests.get(url, params=payload)
     if res.status_code == 200:
-        logger.info("Message send to Wechat successfully. Payload:{payload}. Status code:{code}.".format(payload=payload, code=res.status_code))
+        logger.info(
+            "Message send to Wechat successfully. Payload:{payload}. Status code:{code}.".format(payload=payload,
+                                                                                                 code=res.status_code))
     else:
-        logger.error("Message send to Wechat failed. Payload:{payload}. Status code:{code}.".format(payload=payload, code=res.status_code))
+        logger.error("Message send to Wechat failed. Payload:{payload}. Status code:{code}.".format(payload=payload,
+                                                                                                    code=res.status_code))
 
 
 def get_date_url():
@@ -59,21 +50,37 @@ def get_date_url():
     return date_url
 
 
-def is_register_success():
-    result = register()[1]
-    # return True
-    if  "已存在" in result:
-        logger.info("Register successfully.")
+def is_registered():
+    url = "https://xsswzx.cdu.edu.cn/ispstu1-2/com_user/project.asp?id={id}".format(
+        id=global_config.getRaw('config', 'id'))
+    headers = {
+        "Cookie": global_config.getRaw('config', 'cookie'),
+        "Referer": "https://xsswzx.cdu.edu.cn/ispstu1-2/com_user/left.asp",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36 Edg/88.0.705.50"
+    }
+    res = requests.get(url=url, headers=headers)
+    res.encoding = "utf-8"
+    if res.status_code == 200:
+        logger.info("GET request successfully. URL:{url}. Status code:{code}".format(url=url, code=res.status_code))
+    else:
+        logger.error("GET request failed. URL:{url}. Status code:{code}".format(url=url, code=res.status_code))
+        return
+    latest_date = re.findall(r"(?<=<td class=\"tdmenu\"><div align=\"center\">).*?(?=</div></td>)", res.text)[0]
+    today = datetime.datetime.now()
+    date_ZH = "{y}年{m}月{d}日".format(y=today.year, m=today.month, d=today.day)
+    print(latest_date, date_ZH)
+    if latest_date == date_ZH:
+        logger.info("Register is alread existed.")
         return True
     else:
-        logger.info("Register failed.")
+        logger.info("Register is not existed.")
         return False
 
 
 def main():
-    if register()[0] == False:
-        sleep(2)
-        if is_register_success():
+    if not is_registered():
+        register()
+        if is_registered():
             if global_config.getRaw('messenger', 'enable') == 'true':
                 message = "{time}打卡成功".format(time=datetime.datetime.now())
                 send_wechat("打卡成功", message)
@@ -81,11 +88,11 @@ def main():
             if global_config.getRaw('messenger', 'enable') == 'true':
                 message = "{time}打卡失败".format(time=datetime.datetime.now())
                 send_wechat("打卡失败", message)
-    elif register()[0] == True:
+    else:
         if global_config.getRaw('messenger', 'enable') == 'true':
-                message = "{time}打卡已存在".format(time=datetime.datetime.now())
-                send_wechat("打卡已存在", message)
+            message = "{time}打卡已存在".format(time=datetime.datetime.now())
+            send_wechat("打卡已存在", message)
 
 
-if  __name__ == '__main__':
+if __name__ == '__main__':
     main()
