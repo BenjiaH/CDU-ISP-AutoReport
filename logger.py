f74836c743e4781da2a9678219e0c5e75808677d
benjia <benjia.h@qq.com>
2021-01-24 23:01:37 +0800

    change log format del comment

--- logger.py
+++ logger.py
@@ -1,16 +1,13 @@
 import logging
 import logging.handlers
-'''
-日志模块
-'''
+
 LOG_FILENAME = 'log.log'
 logger = logging.getLogger()
 
 
 def set_logger():
     logger.setLevel(logging.INFO)
-    formatter = logging.Formatter('%(asctime)s - %(process)d-%(threadName)s - '
-                                  '%(pathname)s[line:%(lineno)d] - %(levelname)s: %(message)s')
+    formatter = logging.Formatter('%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s')
     console_handler = logging.StreamHandler()
     console_handler.setFormatter(formatter)
     logger.addHandler(console_handler)
@@ -18,5 +15,6 @@ def set_logger():
         LOG_FILENAME, maxBytes=10485760, backupCount=5, encoding="utf-8")
     file_handler.setFormatter(formatter)
     logger.addHandler(file_handler)
+
 
 set_logger()
