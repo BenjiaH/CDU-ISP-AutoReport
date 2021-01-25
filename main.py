e8c4b67fbb2dcc37054c650beaea4e5c3ec50218
benjia <benjia.h@qq.com>
2021-01-25 23:34:03 +0800

    add enable/disable set time

--- main.py
+++ main.py
@@ -6,13 +6,17 @@ from COVID19_auto_register import main as CAR_main
 from time import sleep
 
 
-while True:
-    now_time = datetime.datetime.now()
-    str_now_time = "{h}.{m}".format(h=now_time.hour, m=now_time.minute)
-    str_set_time = global_config.getRaw('config', 'set_time')
-    if  str_now_time == str_set_time:
-        logger.info("Time arrived. Start to report.")
-        CAR_main()
-        sleep(65)
-    else:
-        sleep(1)
+logger.info("Software started.")
+if global_config.getRaw('config', 'enable') == 'true':
+    while True:
+        now_time = datetime.datetime.now()
+        str_now_time = "{h}.{m}".format(h=now_time.hour, m=now_time.minute)
+        str_set_time = global_config.getRaw('config', 'set_time')
+        if  str_now_time == str_set_time:
+            logger.info("Time arrived. Start to report.")
+            CAR_main()
+            sleep(65)
+        else:
+            sleep(1)
+else:
+    CAR_main()
