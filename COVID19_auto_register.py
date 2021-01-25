e74956b90ae87587411450982b1883ac240e25b8
benjia <benjia.h@qq.com>
2021-01-25 22:24:51 +0800

    optimized use id and password to report optimized change function name optimized wechat push

--- COVID19_auto_register.py
+++ COVID19_auto_register.py
@@ -7,18 +7,61 @@ from time import sleep
 from config import global_config
 from logger import logger
 
+session = requests.Session()
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36 Edg/88.0.705.50",
+}
 
-def register():
-    url = "https://xsswzx.cdu.edu.cn/ispstu1-2/com_user/project_addx.asp?id={id}&id2={id2}".format(
-        id=global_config.getRaw('config', 'id'), id2=get_date_url())
-    headers = {
-        "Cookie": global_config.getRaw('config', 'cookie'),
-        "Referer": "https://xsswzx.cdu.edu.cn/ispstu1-2/com_user/project.asp?id={id}".format(
-            id=global_config.getRaw('config', 'id')),
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36 Edg/88.0.705.50"
-    }
-    res = requests.get(url=url, headers=headers)
+
+def get_captcha_code():
+    url = "https://xsswzx.cdu.edu.cn/ispstu1-2/com_user/weblogin.asp"
+    res = session.get(url=url, headers=headers)
     res.encoding = "utf-8"
+    get_captcha_index = res.text.find('placeholder="验证码"')
+    return (res.text[get_captcha_index + 30: get_captcha_index + 34])
+
+
+def login(code):
+    url = "https://xsswzx.cdu.edu.cn/ispstu1-2/com_user/weblogin.asp"
+    data = {
+        "username": global_config.getRaw('config', 'studentID'),
+        "userpwd": global_config.getRaw('config', 'password'),
+        "code": code,
+        "login": "login",
+        "checkcode": "1",
+        "rank": "0",
+        "action": "login",
+        "m5": "1",
+    }
+    res = session.post(url=url, headers=headers, data=data)
+    if res.status_code == 200:
+        logger.info("POST request successfully. URL:{url}. Status code:{code}".format(url=url, code=res.status_code))
+    else:
+        logger.error("POST request failed. URL:{url}. Status code:{code}".format(url=url, code=res.status_code))
+    return res.text
+
+
+def get_id():
+    url = "https://xsswzx.cdu.edu.cn/ispstu1-2/com_user/left.asp"
+    res = session.get(url=url, headers=headers)
+    if res.status_code == 200:
+        logger.info("GET request successfully. URL:{url}. Status code:{code}".format(url=url, code=res.status_code))
+    else:
+        logger.error("GET request failed. URL:{url}. Status code:{code}".format(url=url, code=res.status_code))
+    res.encoding = "utf-8"
+    try:
+        id = re.findall(r"(?<=id=).*?(?=\">我的事务<)", res.text)[0]
+    except Exception as e:
+        logger.error("Regular expression match failed.[{e}]".format(e=e))
+    with open("leftpage.html", "w", encoding="utf-8") as fp:
+        fp.write(res.text)
+    return id
+
+
+def report(id):
+    url = "https://xsswzx.cdu.edu.cn/ispstu1-2/com_user/project_addx.asp?id={id}&id2={id2}".format(
+        id=id, id2=get_date_url())
+    res = session.get(url=url, headers=headers)
     if res.status_code == 200:
         logger.info("GET request successfully. URL:{url}. Status code:{code}".format(url=url, code=res.status_code))
     else:
@@ -27,13 +70,12 @@ def register():
 
 
 def send_wechat(title, message):
-    """推送信息到微信"""
     url = 'http://sc.ftqq.com/{}.send'.format(global_config.getRaw('messenger', 'sckey'))
     payload = {
         "text": title,
         "desp": message
     }
-    res = requests.get(url, params=payload)
+    res = requests.get(url=url, params=payload)
     if res.status_code == 200:
         logger.info(
             "Message send to Wechat successfully. Payload:{payload}. Status code:{code}.".format(payload=payload,
@@ -50,48 +92,55 @@ def get_date_url():
     return date_url
 
 
-def is_registered():
+def is_reported(id):
     url = "https://xsswzx.cdu.edu.cn/ispstu1-2/com_user/project.asp?id={id}".format(
-        id=global_config.getRaw('config', 'id'))
-    headers = {
-        "Cookie": global_config.getRaw('config', 'cookie'),
-        "Referer": "https://xsswzx.cdu.edu.cn/ispstu1-2/com_user/left.asp",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36 Edg/88.0.705.50"
-    }
-    res = requests.get(url=url, headers=headers)
-    res.encoding = "utf-8"
+        id=id)
+    res = session.get(url=url, headers=headers)
     if res.status_code == 200:
         logger.info("GET request successfully. URL:{url}. Status code:{code}".format(url=url, code=res.status_code))
     else:
         logger.error("GET request failed. URL:{url}. Status code:{code}".format(url=url, code=res.status_code))
         return
-    latest_date = re.findall(r"(?<=<td class=\"tdmenu\"><div align=\"center\">).*?(?=</div></td>)", res.text)[0]
+    res.encoding = "utf-8"
+    try:
+        latest_date = re.findall(r"(?<=<td class=\"tdmenu\"><div align=\"center\">).*?(?=</div></td>)", res.text)[0]
+    except Exception as e:
+        logger.error("Regular expression match failed.[{e}]".format(e=e))
     today = datetime.datetime.now()
     date_ZH = "{y}年{m}月{d}日".format(y=today.year, m=today.month, d=today.day)
-    print(latest_date, date_ZH)
     if latest_date == date_ZH:
-        logger.info("Register is alread existed.")
+        logger.info("Report is alread existed.")
         return True
     else:
-        logger.info("Register is not existed.")
+        logger.info("Report is not existed.")
         return False
 
 
 def main():
-    if not is_registered():
-        register()
-        if is_registered():
+    captcha_code = get_captcha_code()
+    login(captcha_code)
+    id = get_id()
+    if not is_reported(id):
+        report(id)
+        if is_reported(id):
+            logger.info(
+                "Report successfully. ID:{studentID}".format(studentID=global_config.getRaw('config', 'studentID')))
             if global_config.getRaw('messenger', 'enable') == 'true':
-                message = "{time}打卡成功".format(time=datetime.datetime.now())
-                send_wechat("打卡成功", message)
+                message = "{time}打卡成功!学号：{studentID}".format(time=datetime.datetime.now(),
+                                                             studentID=global_config.getRaw('config', 'studentID'))
+                send_wechat("打卡成功!", message)
         else:
+            logger.error(logger.info(
+                "Report failed. ID:{studentID}".format(studentID=global_config.getRaw('config', 'studentID'))))
             if global_config.getRaw('messenger', 'enable') == 'true':
-                message = "{time}打卡失败".format(time=datetime.datetime.now())
-                send_wechat("打卡失败", message)
+                message = "{time}打卡失败!学号：{studentID}".format(time=datetime.datetime.now(),
+                                                             studentID=global_config.getRaw('config', 'studentID'))
+                send_wechat("打卡失败!", message)
     else:
         if global_config.getRaw('messenger', 'enable') == 'true':
-            message = "{time}打卡已存在".format(time=datetime.datetime.now())
-            send_wechat("打卡已存在", message)
+            message = "{time}打卡已存在!学号：{studentID}".format(time=datetime.datetime.now(),
+                                                          studentID=global_config.getRaw('config', 'studentID'))
+            send_wechat("打卡已存在!", message)
 
 
 if __name__ == '__main__':
