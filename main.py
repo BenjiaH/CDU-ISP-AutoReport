67ff483b6abb926d222b3da8027c594cbab17d27
benjia <benjia.h@qq.com>
2021-02-04 23:04:58 +0800

    changed:push moved to top main module

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import datetime
 import security
 
 from logger import logger
-logger.info("Software started.")
 from config import global_config
 from time import sleep, time
 from account import global_account
 from auto_report import main as ar_main
+from push import global_push
 
 
 def main():
@@ -45,9 +45,12 @@ def main():
 
 def single_mode():
     logger.info("Reporting... ID:{studentID}.".format(studentID=global_config.getRaw('account', 'studentID')))
-    ar_main(studentID=global_config.getRaw('account', 'studentID'), password=global_config.getRaw('account', 'password'),
-            wechat_push=global_config.getRaw('config', 'wechat_enable'), email_push=global_config.getRaw('config', 'email_enable'), 
-            sckey=global_config.getRaw('messenger', 'sckey'),email_rever=global_config.getRaw('messenger', 'email'))
+    ret = ar_main(uid=global_config.getRaw('account', 'studentID'),
+                  password=global_config.getRaw('account', 'password'))
+    push(ret, uid=global_config.getRaw('account', 'studentID'),
+         wechat_push=global_config.getRaw('config', 'wechat_enable'),
+         email_push=global_config.getRaw('config', 'email_enable'), sckey=global_config.getRaw('messenger', 'sckey'),
+         email_rxer=global_config.getRaw('messenger', 'email'))
 
 
 def multiple_mode():
@@ -55,9 +58,10 @@ def multiple_mode():
     n = global_account.row
     for i in range(n):
         logger.info("{i}/{n} Reporting... ID:{studentID}.".format(i=i + 1, n=n, studentID=global_account.studentID[i]))
-        ar_main(studentID = global_account.studentID[i], password=global_account.password[i],
-                wechat_push=global_account.wechat_push[i], email_push=global_account.email_push[i],
-                sckey=global_account.sckey[i],email_rever=global_account.email[i])
+        ret = ar_main(uid=global_account.studentID[i], password=global_account.password[i])
+        push(ret, uid=global_account.studentID[i], wechat_push=global_account.wechat_push[i],
+             email_push=global_account.email_push[i], sckey=global_account.sckey[i],
+             email_rxer=global_account.email[i])
         sleep(1.5)
 
 
@@ -72,6 +76,39 @@ def report_task():
         multiple_mode()
     end_time = time()
     logger.info("Report completed. Cost time:{:.2f}s.".format(end_time - start_time))
+
+
+def push(result, uid, wechat_push, email_push, sckey="", email_rxer=""):
+    pass
+    global_wechat = global_config.getRaw('config', 'wechat_enable')
+    global_email = global_config.getRaw('config', 'email_enable')
+    if result == 0:
+        title = "打卡已存在!"
+        message = "{time}打卡已存在!学号：{uid}".format(time=datetime.datetime.now(), uid=uid)
+        if global_wechat == "true":
+            if wechat_push == "1" or wechat_push == "true":
+                global_push.wechat(title, message, sckey)
+        if global_email == "true":
+            if email_push == "1" or email_push == "true":
+                global_push.bot_email.send(title, message, [email_rxer])
+    elif result == 1:
+        title = "打卡成功!"
+        message = "{time}打卡成功!学号：{uid}".format(time=datetime.datetime.now(), uid=uid)
+        if global_wechat == "true":
+            if wechat_push == "1" or wechat_push == "true":
+                global_push.wechat(title, message, sckey)
+        if global_email == "true":
+            if email_push == "1" or email_push == "true":
+                global_push.bot_email.send(title, message, [email_rxer])
+    elif result == 2:
+        title = "打卡失败!"
+        message = "{time}打卡失败,请手动打卡!学号：{uid}".format(time=datetime.datetime.now(), uid=uid)
+        if global_wechat == "true":
+            if wechat_push == "1" or wechat_push == "true":
+                global_push.wechat(title, message, sckey)
+        if global_email == "true":
+            if email_push == "1" or email_push == "true":
+                global_push.bot_email.send(title, message, [email_rxer])
 
 
 if __name__ == '__main__':
