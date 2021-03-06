acbf2bf41389dc8f83d9062bf8dfb4c7855404ef
benjia <benjia.h@qq.com>
2021-03-06 22:13:47 +0800

    Changed:class Report

--- common/report.py
+++ common/report.py
@@ -3,119 +3,114 @@ import re
 
 from datetime import datetime
 from common import security
-from urllib import parse
 from common.logger import logger
 
-session = 0
-host = 0
-headers = 0
 
+class Report:
+    def __init__(self):
+        self._session = 0
+        self._host = 0
+        self._headers = 0
+        self._main_host = "https://xsswzx.cdu.edu.cn/"
+        self._id_value = 0
+        self._date = self._get_date()
 
-def get_captcha_code():
-    url = "{host}/com_user/weblogin.asp".format(host=host)
-    res = session.get(url=url, headers=headers)
-    res.encoding = "utf-8"
-    captcha_index = res.text.find('placeholder="验证码"')
-    return res.text[captcha_index + 30: captcha_index + 34]
+    @staticmethod
+    def _get_date():
+        today = datetime.now()
+        today = "{y}年{m}月{d}日".format(y=today.year, m=today.month, d=today.day)
+        return today
 
+    def _get_captcha_code(self):
+        url = "{host}/com_user/weblogin.asp".format(host=self._host)
+        res = self._session.get(url=url, headers=self._headers)
+        res.encoding = "utf-8"
+        captcha_index = res.text.find('placeholder="验证码"')
+        return res.text[captcha_index + 30: captcha_index + 34]
 
-def login(uid, password, code):
-    url = "{host}/com_user/weblogin.asp".format(host=host)
-    data = {
-        "username": uid,
-        "userpwd": password,
-        "code": code,
-        "login": "login",
-        "checkcode": "1",
-        "rank": "0",
-        "action": "login",
-        "m5": "1",
-    }
-    res = session.post(url=url, headers=headers, data=data)
-    if res.status_code != 200:
-        logger.error("POST request failed. URL:{url}. Status code:{code}".format(url=url, code=res.status_code))
+    def _login(self, uid, password, code):
+        url = "{host}/com_user/weblogin.asp".format(host=self._host)
+        data = {
+            "username": uid,
+            "userpwd": password,
+            "code": code,
+            "login": "login",
+            "checkcode": "1",
+            "rank": "0",
+            "action": "login",
+            "m5": "1",
+        }
+        res = self._session.post(url=url, headers=self._headers, data=data)
+        if res.status_code != 200:
+            logger.error("POST request failed. URL:{url}. Status code:{code}".format(url=url, code=res.status_code))
 
+    def _get_id(self):
+        url = "{host}/com_user/left.asp".format(host=self._host)
+        res = self._session.get(url=url, headers=self._headers)
+        if res.status_code != 200:
+            logger.error("GET request failed. URL:{url}. Status code:{code}".format(url=url, code=res.status_code))
+        res.encoding = "utf-8"
+        try:
+            self._id_value = re.findall(r"(?<=id=).*?(?=\">我的事务<)", res.text)[0]
+            logger.info("Login successfully.")
+        except Exception as e:
+            self._id_value = 0
+            logger.error("Regular expression match failed.[{e}]".format(e=e))
+            logger.error("Login failed.")
 
-def get_id():
-    url = "{host}/com_user/left.asp".format(host=host)
-    res = session.get(url=url, headers=headers)
-    if res.status_code != 200:
-        logger.error("GET request failed. URL:{url}. Status code:{code}".format(url=url, code=res.status_code))
-    res.encoding = "utf-8"
-    try:
-        id = re.findall(r"(?<=id=).*?(?=\">我的事务<)", res.text)[0]
-        logger.info("Login successfully.")
-        return id
-    except Exception as e:
-        id = 0
-        logger.error("Regular expression match failed.[{e}]".format(e=e))
-        logger.error("Login failed.")
-        return id
+    def _report(self):
+        if self._id_value == 0:
+            return
+        url = "{host}/com_user/project_addx.asp?id={id}&id2={id2}".format(
+            host=self._host, id=self._id_value, id2=self._date)
+        logger.info("Get report url.")
+        res = self._session.get(url=url, headers=self._headers)
+        if res.status_code != 200:
+            logger.error("GET request failed. URL:{url}. Status code:{code}".format(url=url, code=res.status_code))
 
-
-def report(id):
-    if id == 0:
-        return
-    url = "{host}/com_user/project_addx.asp?id={id}&id2={id2}".format(
-        host=host, id=id, id2=get_date())
-    logger.info("Get report url.")
-    res = session.get(url=url, headers=headers)
-    if res.status_code != 200:
-        logger.error("GET request failed. URL:{url}. Status code:{code}".format(url=url, code=res.status_code))
-
-
-def get_date():
-    today = datetime.now()
-    today = "{y}年{m}月{d}日".format(y=today.year, m=today.month, d=today.day)
-    # today = parse.quote(today)
-    return today
-
-
-def is_reported(id):
-    if id == 0:
-        return
-    url = "{host}/com_user/project.asp?id={id}".format(
-        host=host, id=id)
-    res = session.get(url=url, headers=headers)
-    if res.status_code != 200:
-        logger.error("GET request failed. URL:{url}. Status code:{code}".format(url=url, code=res.status_code))
-        return
-    res.encoding = "utf-8"
-    # host refreshes records every months(1st day of this month)
-    if "还没有登记记录" in res.text:
-        logger.info("Report is not existed.")
-        return False
-    try:
-        latest_date = re.findall(r"(?<=<td class=\"tdmenu\"><div align=\"center\">).*?(?=</div></td>)", res.text)[0]
-    except Exception as e:
-        logger.error("Regular expression match failed.[{e}]".format(e=e))
-        return
-    if latest_date == get_date():
-        logger.info("Report is existed.")
-        return True
-    else:
-        logger.info("Report is not existed.")
-        return False
-
-
-def main(uid, password):
-    global session, host, headers
-    session = requests.Session()
-    host = "https://xsswzx.cdu.edu.cn/" + security.get_random_host()
-    headers = {
-        "User-Agent": security.get_random_useragent()
-    }
-    captcha_code = get_captcha_code()
-    login(uid, password, captcha_code)
-    id = get_id()
-    if is_reported(id):
-        logger.info("Report is already existed. ID:{uid}".format(uid=uid))
-        return 0
-    else:
-        report(id)
-        if is_reported(id):
-            logger.info("Report successfully. ID:{uid}".format(uid=uid))
-            return 1
+    def _is_reported(self):
+        if self._id_value == 0:
+            return
+        url = "{host}/com_user/project.asp?id={id}".format(
+            host=self._host, id=self._id_value)
+        res = self._session.get(url=url, headers=self._headers)
+        if res.status_code != 200:
+            logger.error("GET request failed. URL:{url}. Status code:{code}".format(url=url, code=res.status_code))
+            return
+        res.encoding = "utf-8"
+        # host refreshes records every months(1st day of this month)
+        if "还没有登记记录" in res.text:
+            logger.info("Report is not existed.")
+            return False
+        try:
+            latest_date = re.findall(r"(?<=<td class=\"tdmenu\"><div align=\"center\">).*?(?=</div></td>)", res.text)[0]
+        except Exception as e:
+            logger.error("Regular expression match failed.[{e}]".format(e=e))
+            return
+        if latest_date == self._date:
+            logger.info("Report is existed.")
+            return True
         else:
-            logger.error("Report failed. ID:{uid}".format(uid=uid))
-            return 2
+            logger.info("Report is not existed.")
+            return False
+
+    def main(self, uid, password):
+        self._session = requests.Session()
+        self._host = self._main_host + security.get_random_host()
+        self._headers = {
+            "User-Agent": security.get_random_useragent()
+        }
+        captcha_code = self._get_captcha_code()
+        self._login(uid, password, captcha_code)
+        self._get_id()
+        if self._is_reported():
+            logger.info("Report is already existed. ID:{uid}".format(uid=uid))
+            return 0
+        else:
+            self._report()
+            if self._is_reported():
+                logger.info("Report successfully. ID:{uid}".format(uid=uid))
+                return 1
+            else:
+                logger.error("Report failed. ID:{uid}".format(uid=uid))
+                return 2
