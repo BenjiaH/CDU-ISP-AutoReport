5fe22b956618ec56c3c9b138b608c82258202ca8
benjia <benjia.h@qq.com>
2021-03-06 22:14:05 +0800

    Changed:class Report

--- common/service.py
+++ common/service.py
@@ -4,7 +4,7 @@ from common import security
 from common.logger import logger
 from common.config import global_config
 from common.account import global_account
-from common.report import main as ar_main
+from common.report import Report
 from common.push import global_push
 
 
@@ -19,6 +19,7 @@ class ReportService:
         self._email_push = global_config.getRaw('config', 'email_enable')
         self._sckey = global_config.getRaw('messenger', 'sckey')
         self._email_rxer = global_config.getRaw('messenger', 'email')
+        self._report = Report()
 
     def _get_now_time(self):
         now = datetime.now()
@@ -27,18 +28,17 @@ class ReportService:
 
     def _single_mode(self):
         logger.info("Report ID:{studentID}.".format(studentID=self._uid))
-        ret = ar_main(uid=self._uid, password=self._password)
+        ret = self._report.main(uid=self._uid, password=self._password)
         global_push.push(ret, uid=self._uid, wechat_push=self._wechat_push, email_push=self._email_push,
                          sckey=self._sckey, email_rxer=self._email_rxer)
 
-    @staticmethod
-    def _multiple_mode():
+    def _multiple_mode(self):
         global_account.refresh()
         n = global_account.row
         for i in range(n):
             logger.info(
                 "{i}/{n} Report ID:{studentID}.".format(i=i + 1, n=n, studentID=global_account.studentID[i]))
-            ret = ar_main(uid=global_account.studentID[i], password=global_account.password[i])
+            ret = self._report.main(uid=global_account.studentID[i], password=global_account.password[i])
             global_push.push(ret, uid=global_account.studentID[i], wechat_push=global_account.wechat_push[i],
                              email_push=global_account.email_push[i], sckey=global_account.sckey[i],
                              email_rxer=global_account.email[i])
@@ -86,6 +86,3 @@ class ReportService:
                         logger.info("Cleaning... Estimated:1min")
                         sleep(60)
                         break
-
-
-report_service = ReportService()
