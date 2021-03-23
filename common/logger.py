7426b0998bc6f242b0813bf8bff9589ec253e5e6
benjia <benjia.h@qq.com>
2021-03-23 21:40:36 +0800

    Added:enable loguru

--- common/logger.py
+++ common/logger.py
@@ -1,32 +1,39 @@
 import os
-import logging
-import logging.handlers
+import sys
 
-os.chdir(os.path.dirname(__file__))
-LOG_FILENAME = os.path.abspath(r"../log.log")
-logger = logging.getLogger()
+from loguru import logger
 
 
-def set_logger():
-    logger.setLevel(logging.INFO)
-    formatter = logging.Formatter('%(asctime)s [%(levelname)5s] %(filename)s[line:%(lineno)d]: %(message)s')
-    console_handler = logging.StreamHandler()
-    console_handler.setFormatter(formatter)
-    logger.addHandler(console_handler)
-    file_handler = logging.handlers.RotatingFileHandler(
-        LOG_FILENAME, maxBytes=10485760, backupCount=5, encoding="utf-8")
-    file_handler.setFormatter(formatter)
-    logger.addHandler(file_handler)
+class Logger:
+    def __init__(self, log_file: str, debug_file: str):
+        self._log_fmt = "{time:YYYY-MM-DD HH:mm:ss.SSS} [<level>{level:<5}</level>]{file}.{line}: {message}"
+        self._debug_fmt = "{time:YYYY-MM-DD HH:mm:ss.SSS} [<level>{level:}</level>] {name}:{function}:{line}: {message}"
+        os.chdir(os.path.dirname(__file__))
+        logger.remove()
+        logger.add(sink=log_file, filter=self.run_filter, format=self._log_fmt)
+        logger.add(sink=sys.stderr, filter=self.run_filter, format=self._log_fmt)
+        logger.add(sink=debug_file, filter=self.debug_filter, format=self._debug_fmt)
+        logger.info("Logger started.")
+        self.logger = logger
+
+    @staticmethod
+    def debug_filter(record):
+        return record["level"].name == "DEBUG"
+
+    @staticmethod
+    def run_filter(record):
+        return record["level"].name == "INFO" or record["level"].name == "ERROR"
+
+    @staticmethod
+    def log_version(stage: str, version=""):
+        commit_id = ""
+        if os.path.exists("../.git"):
+            commit_id = (os.popen("git rev-parse --short HEAD").read()).replace("\n", "")
+            version += "."
+        info = version + commit_id + "(" + stage + ")"
+        logger.info("Version:{version}".format(version=info))
 
 
-def log_version(stage: str, version=""):
-    commit_id = ""
-    if os.path.exists("../.git"):
-        commit_id = (os.popen("git rev-parse --short HEAD").read()).replace("\n", "")
-    version += "."
-    info = version + commit_id + "(" + stage + ")"
-    logger.info("Version:{version}".format(version=info))
-
-
-set_logger()
-logger.info("Logger started.")
+handlers = Logger("../log.log", "../debug.log")
+logger = handlers.logger
+log_version = handlers.log_version
