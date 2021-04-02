0ca37883d39b105c167f2296974f3df60c955bb2
benjia <benjia.h@qq.com>
2021-04-02 17:54:14 +0800

    Changed:version to 1.1.0.release

--- main.py
+++ main.py
@@ -12,6 +12,6 @@ logo = r"""
                                                                                      |_|                   
 """
 print(logo)
-log_version("rc", "1.1.0")
+log_version("release", "1.1.0")
 report_service = ReportService()
 report_service.start()
