745544de35a691772fe6cc148e76896f532ce7cc
benjia <benjia.h@qq.com>
2022-02-20 11:20:50 +0800

    Fixed:- optimized the report process - remove unused code

--- common/report.py
+++ common/report.py
@@ -15,7 +15,6 @@ class Report:
         self._session = 0
         self._host = 0
         self._headers = 0
-        self._main_host = "https://xsswzx.cdu.edu.cn/"
         self._navigation_url = 0
         self._report_url = 0
         self._date = ""
@@ -73,16 +72,22 @@ class Report:
         }
         res = self._session.post(url=url, headers=self._headers, data=payload)
         res.encoding = "utf-8"
-        logger.debug(f"URL:{url}. Status code:{res.status_code}")
+        logger.debug(f"URL:{url}. Payload:{payload}. Status code:{res.status_code}")
         if res.status_code != 200:
             logger.error(f"Failed:POST request. URL:{url}. Status code:{res.status_code}")
-        if "学号或密码错误" in res.text:
-            logger.error("Failed to login the ISP.[Incorrect username or password]")
-            logger.debug("Failed to login the ISP.[Incorrect username or password]")
             self._errno = 2
             logger.debug(f"Set the error code: {self._errno}.")
             self._error = 1
             logger.debug(f"Set the error flag: {self._error}.")
+        elif "重新登陆" in res.text:
+            logger.error("Failed to login the ISP.[Incorrect username, password or captcha code]")
+            logger.debug("Failed to login the ISP.[Incorrect username, password or captcha code]")
+            self._errno = 2
+            logger.debug(f"Set the error code: {self._errno}.")
+            self._error = 1
+            logger.debug(f"Set the error flag: {self._error}.")
+        else:
+            logger.info("Successful to login the ISP.")
 
     @logger.catch
     def _get_navigation_url(self, target):
@@ -98,40 +103,12 @@ class Report:
         try:
             soup = BeautifulSoup(res.text, 'lxml')
             self._navigation_url = soup.find('a', string=target)['href']
-            logger.info("Successful to login the ISP.")
-            logger.debug(f"Project url:{self._navigation_url}.")
+            logger.debug(f'Navigation "{target}" url:{self._navigation_url}.')
         except Exception as e:
             logger.error(f"Failed to get the project url.[{e}]")
             self._error = 1
             logger.debug(f"Set the error flag: {self._error}.")
             self._errno = 3
-            logger.debug(f"Set the error code: {self._errno}.")
-            logger.debug("{url} content:\n{res}".format(url=url, res=re.sub(r"\n|\r|\t|\s", "", res.text)))
-
-    @logger.catch
-    def _get_report_url(self):
-        if self._error == 1:
-            logger.debug(f"The error flag: {self._error}. Exit the function.")
-            return
-        url = f"{self._host}/{self._navigation_url}"
-        res = self._session.get(url=url, headers=self._headers)
-        logger.debug(f"URL:{url}. Status code:{res.status_code}")
-        if res.status_code != 200:
-            logger.error(f"Failed:GET request. URL:{url}. Status code:{res.status_code}")
-        res.encoding = "utf-8"
-        try:
-            soup = BeautifulSoup(res.text, 'lxml')
-            t_url = str(soup.find_all("script", type="text/javascript")[2]).replace("\r\n", "")
-            report_url = t_url[t_url.find("href=") + 6: -11]
-            self._report_url = report_url.replace('"+adds2+"', "undefined").replace('"+addsxy2', "undefined")
-            logger.info("Get the report url.")
-            logger.debug(f"The report url:{self._report_url}.")
-        except Exception as e:
-            logger.error(f"Failed to get the report url.")
-            logger.error(f'[{e.__traceback__.tb_frame.f_globals["__file__"]}.{e.__traceback__.tb_lineno}:{e}]')
-            self._error = 1
-            logger.debug(f"Set the error flag: {self._error}.")
-            self._errno = 4
             logger.debug(f"Set the error code: {self._errno}.")
             logger.debug("{url} content:\n{res}".format(url=url, res=re.sub(r"\n|\r|\t|\s", "", res.text)))
 
@@ -149,7 +126,7 @@ class Report:
             "addsxy": "undefined"
         }
         res = self._session.get(url=url, headers=self._headers, data=payload)
-        logger.debug(f"URL:{url}. Status code:{res.status_code}")
+        logger.debug(f"URL:{url}. Payload:{payload}. Status code:{res.status_code}")
         if res.status_code != 200:
             logger.error(f"Failed:GET request. URL:{url}. Status code:{res.status_code}")
         res.encoding = "utf-8"
@@ -178,7 +155,7 @@ class Report:
             "addsxy": "undefined"
         }
         res = self._session.get(url=url, headers=self._headers, data=payload)
-        logger.debug(f"URL:{url}. Status code:{res.status_code}")
+        logger.debug(f"URL:{url}. Payload:{payload}. Status code:{res.status_code}")
         if res.status_code != 200:
             logger.error(f"Failed:GET request. URL:{url}. Status code:{res.status_code}")
         res.encoding = "utf-8"
@@ -189,7 +166,7 @@ class Report:
         self._error = 0
         self._errno = 0
         self._session = requests.Session()
-        self._host = self._main_host + security.get_random_host() + "/com_user"
+        self._host = "https://xsswzx.cdu.edu.cn/" + security.get_random_host() + "/com_user"
         self._headers = {
             "User-Agent": security.get_random_useragent()
         }
