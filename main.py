e6feb6ce78023f72a51428f55a1643364ff241e6
benjia <benjia.h@qq.com>
2022-03-07 19:22:30 +0800

    Changed:version

--- main.py
+++ main.py
@@ -12,6 +12,6 @@ LOGO = r"""
                                                                                      |_|                   
 """
 print(LOGO)
-log_version("beta", "1.3.0")
+log_version("rc", "1.3.0")
 report_service = ReportService()
 report_service.start()
